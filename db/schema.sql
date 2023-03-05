615624a77c52a0782beaf0e1863b21d1206b3e61
Fifinella <h.hoesley@gmail.com>
2023-03-04 16:04:49 -0800

    corrected syntax errors

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,8 @@
 DROP DATABASE IF EXISTS employeedb;
 CREATE DATABASE employeedb;
 
+USE employeedb;
+
 
 CREATE TABLE department (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
@@ -11,7 +13,7 @@ CREATE TABLE role (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL UNSIGNED NOT NULL, 
-    department_id INT NOT NULL,
+    department_id INT UNSIGNED NOT NULL,
     INDEX dep_ind (department_id),
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE 
 
@@ -26,5 +28,5 @@ CREATE TABLE employee (
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
     manager_id INT UNSIGNED, 
     INDEX man_ind (manager_id),
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES manager(id) ON DELETE SET NULL 
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL 
 );
