3fb34f8204ce242e0a73a73346f297caff9df2f6
Fifinella <h.hoesley@gmail.com>
2023-03-20 12:45:41 -0700

    added employee functions to index.js

--- index.js
+++ index.js
@@ -118,4 +118,126 @@ function createRole() {
       });
   });
 }
+
+function createEmployee() {
+  inquirer
+  .prompt([
+    {
+      name: "first_name",
+      message: "First name?",
+    },
+    {
+      name: "last_name",
+      message: "Last name?",
+    }
+  ])
+  .then((res) => {
+    let firstName = res.first_name;
+    let lastName = res.last_name;
+
+    store.viewAllRoles().then(([roles]) => {
+      const roleChoices = roles.map((roles) => {
+        return {
+          name: roleChoices.name,
+          value: roleChoices.id,
+        }
+      }) ;
+
+    })
+    inquirer
+    .prompt({
+      name: "role_id",
+      type: "list",
+      message: "Employee's role?",
+      choices: roleChoices, 
+    })
+    .then((res) => {
+      let roleId = res.role_id;
+
+      store.getAllDepartments().then(([departments]) => {
+        const managerChoices = employees.map(({ id, first_name, last_name }) => ({
+          return {
+            name: `${first_name} ${last_name}`,
+            value: id,
+          }
+        })
+      })
+      managerChoices.unshift({ name: "None", value: null });
+
+      inquirer
+      .prompt({
+        name: "manager_id",
+        type: "list",
+        message: "Employee's manager?",
+        choices: managerChoices,
+      })
+      .then((res) => {
+        let employee = {
+          first_name: firstName,
+          last_name: lastName,
+          role_id: roleId,
+          manager_id: res.manager_id,
+          is_manager: 0
+        };
+
+        utilQueries.createEmployee(employee);
+      })
+      .then(() =>
+        console.log(`\n ${firstName} ${lastName} has been added! \n`)
+      )
+      .then(() => startApp());
+    })
+  })
+
+  function updateEmployeeRole() {
+    store.viewAllEmployees().then(([employees]) => {
+      let CompanyEmployees = rows;
+      const employeeChoices = CompanyEmployees.map(
+        ({ id, first_name, last_name }) => ({
+          name: `${first_name} ${last_name}`,
+          value: id,
+        })
+      );
+  
+      inquirer
+        .prompt([
+          {
+            name: "employeeId",
+            type: "list",
+            message: "Select an employee",
+            choices: employeeChoices,
+          },
+        ])
+        .then((res) => {
+          let employeeId = res.employeeId;
+          utilQueries.viewAllRoles().then(([rows]) => {
+            let roles = rows;
+            const roleChoices = roles.map(({ id, title }) => ({
+              name: title,
+              value: id,
+            }));
+  
+            inquirer
+              .prompt([
+                {
+                  name: "roleId",
+                  type: "list",
+                  message: "New role name?",
+                  choices: roleChoices,
+                },
+              ])
+              .then((res) =>
+                utilQueries.updateEmployeeRole(employeeId, res.roleId)
+              )
+              .then(() => console.log("The role has been updated"))
+              .then(() => startApp());
+          });
+        });
+    });
+  }
+  
+  function quit() {
+    process.exit();
+}
 mainMenu();
+}
